812a95445a0b29e7bf0dba5033e95cae1fc0b604
Utsav Shahi <shahiutsav180@gmail.com>
2022-03-27 14:10:45 +0545

    Add basic math operations

---
+++ script.js
@@ -0,0 +1,22 @@
+const add = function () {
+    return arguments[0] + arguments[1]
+}
+
+console.log(add(1, 2))
+
+const subtract = function () {
+    return arguments[0] - arguments[1]
+}
+console.log(subtract(2, 1))
+
+const multiply = function () {
+    return arguments[0] * arguments[1]
+}
+
+console.log(multiply(2, 3))
+
+const divide = function () {
+    return arguments[0] / arguments[1]
+}
+
+console.log(divide(4, 2))
