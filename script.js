69622e4540f00123e6e890b466f634218af44466
Utsav Shahi <shahiutsav180@gmail.com>
2022-03-29 16:23:13 +0545

    Add getDisplay function

--- script.js
+++ script.js
@@ -21,11 +21,11 @@ class Calculator {
     }
 
     chooseOperation(operation) {
-        this.operation = operation
         if (this.currentOperand === "") return
         if (this.previousOperand !== "") {
             this.compute()
         }
+        this.operation = operation
         this.previousOperand = this.currentOperand
         this.currentOperand = ""
     }
@@ -56,9 +56,36 @@ class Calculator {
         this.operation = undefined
     }
 
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split(".")[0])
+        const decimalDigits = stringNumber.split(".")[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ""
+        } else {
+            integerDisplay = integerDigits.toLocaleString("en", {
+                maximumFractionDigits: 0,
+            })
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+    }
+
     updateDisplay() {
-        this.currentOperandTextElement.innerText = this.currentOperand
-        this.previousOperandTextElement.innerText = this.previousOperand
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(
+            this.currentOperand
+        )
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText = `${this.getDisplayNumber(
+                this.previousOperand
+            )} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ""
+        }
     }
 }
 
